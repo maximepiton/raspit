d9df70eb7a04f469e7148d211ae248b987c2ae58
Alexandre LAPADU <alexandre.lapadu@delair.aero>
2019-03-09 19:13:19 +0100

    compute paraglidable score

--- paralert/paralert.py
+++ paralert/paralert.py
@@ -3,14 +3,13 @@ import json
 import math
 import yaml
 import urllib.request
+import numpy
 
 
 def get_paraglidable_json():
     
     response = requests.get("https://api.paraglidable.com/?key=d60092fb1105a1f4&format=JSON&version=1")
     forecast_pgble = json.loads(response.text)
-    forecast_pgble = json.dumps(forecast_pgble, sort_keys=True, indent=4)
-#    print(forecast_pgble)
           
     write_to_JSON_file('./paraglidable_forecast/', 'forecast_pgble', forecast_pgble)
 
@@ -98,10 +97,55 @@ def get_flight_score(forecast_df):
     return forecast_df.sum(axis = 0)["pblh"]
 
 
+def date_N_day_after(N):
+    
+    from datetime import datetime, timedelta
+    
+    tomorrow = datetime.now() + timedelta(days=N)
+
+    return tomorrow.strftime("%Y-%m-%d")
+
+
+def get_score_paraglidable(site):
+    
+    with open("./paraglidable_forecast/forecast_pgble.json", "r") as f:
+        pgble_data = json.load(f)
+
+    tm_fly = pgble_data[date_N_day_after(1)][site]['forecast']['fly']
+    tm_XC = pgble_data[date_N_day_after(1)][site]['forecast']['XC']
+    atm_fly = pgble_data[date_N_day_after(2)][site]['forecast']['fly']
+    atm_XC = pgble_data[date_N_day_after(2)][site]['forecast']['XC']
+        
+    tomorrow_score = numpy.mean([tm_fly,tm_XC])
+    after_tomorrow_score = numpy.mean([atm_fly,atm_XC])
+    
+    tm_score_10 = int(tomorrow_score * 10)
+    atm_score_10 = int(after_tomorrow_score * 10)
+    
+    print(pgble_data[date_N_day_after(1)][site]['name'])    
+    print('demain : ' + str(tm_score_10) + '/10')
+    print('apres demain : ' + str(atm_score_10) + '/10')
+
+
 if __name__ == "__main__":
     
+    """
+    Get input forecast
+    """
     get_paraglidable_json()
-    
+
+
+    """
+    Get Paraglidable score
+    """
+    for x in range(0, 3):
+        get_score_paraglidable(x)
+
+
+    """
+    Get Paralert score
+    """
+
     with open("forecast.json", "r") as f:
         json_data = json.load(f)
 
@@ -109,20 +153,20 @@ if __name__ == "__main__":
         locations = yaml.load(f)
 
     site_parameters = locations["gensac"]
-    print(site_parameters)
+#    print(site_parameters)
 
     forecast_df = pd.DataFrame(json_data["data"]).transpose()
     add_windspeed_winddir_to_forecast(forecast_df)
-    print(forecast_df)
+#    print(forecast_df)
 
     forecast_df = filter_out_low_cloudbase(
         forecast_df, site_parameters["min_bl_thickness"]
     )
-    print(forecast_df)
+#    print(forecast_df)
 
     forecast_df = filter_out_strong_wind(
         forecast_df, site_parameters["max_windspeed"]
     )
-    print(forecast_df)
+#    print(forecast_df)
 
-    print(get_flight_score(forecast_df))
+    print('Paralert : ' + str(get_flight_score(forecast_df)))
